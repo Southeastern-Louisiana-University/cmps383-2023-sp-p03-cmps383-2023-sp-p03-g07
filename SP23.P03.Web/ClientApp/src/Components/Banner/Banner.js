700870cc6d52a73de3a52143296ffce4c7338bd3
Kearney Butler <kearneybitler95@gmail.com>
2023-03-08 19:07:50 -0600

    the config stuff

--- SP23.P03.Web/ClientApp/src/Components/Banner/Banner.js
+++ SP23.P03.Web/ClientApp/src/Components/Banner/Banner.js
@@ -1,24 +1,37 @@
-import React from 'react'
-import './Banner.css';
-import { NavLink } from 'react-router-dom';
-import mainLogo from '../Materials/entrack_logo.png';
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+import "./Banner.css";
+import { NavLink } from "react-router-dom";
+import mainLogo from "../Materials/entrack_logo.png";
 
 function Banner() {
-    return (
-        <div className="banner-container">
-            <div className="bannerLogo">
-                <li><NavLink className="navbar-logo" exact to="/"><img src={mainLogo}></img></NavLink></li>
-            </div>
-            <div className="account-buttons">
-                <div className="login-button">
-                    <li><NavLink className="navbar-login" to="/login">Login</NavLink></li>
-                </div>
-                <div className="sign-up-button">
-                    <li><NavLink className="navbar-sign-up" to="/sign-up">Sign Up</NavLink></li>
-                </div>
-            </div>
+  return (
+    <div className="banner-container">
+      <div className="bannerLogo">
+        <li>
+          <NavLink className="navbar-logo" exact to="/">
+            <img src={mainLogo}></img>
+          </NavLink>
+        </li>
+      </div>
+      <div className="account-buttons">
+        <div className="login-button">
+          <li>
+            <NavLink className="navbar-login" to="/login">
+              Login
+            </NavLink>
+          </li>
         </div>
-    )
+        <div className="sign-up-button">
+          <li>
+            <NavLink className="navbar-sign-up" to="/sign-up">
+              Sign Up
+            </NavLink>
+          </li>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
