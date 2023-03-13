f5668c13fda8141e9446fa4a32b45cf1129395cf
cyponos <98052408+cyponos@users.noreply.github.com>
2023-03-13 14:00:56 -0500

    Test
    To validate I can make commits to my branch and my repo is responding.

---
+++ SP23.P03.Web/Controllers/Test.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SP23.P03.Web.Controllers
+{
+    [ApiController]
+    [Route("api/Test")]
+    public class Test
+    {
+        // This is to test my branch and repo since its buggy right now
+    }
+}
