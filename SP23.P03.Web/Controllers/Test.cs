0044dd2c62f475bb75bf31881c9c9d1a406e87d1
cyponos <98052408+cyponos@users.noreply.github.com>
2023-03-13 18:12:34 -0500

    Update Test.cs

--- SP23.P03.Web/Controllers/Test.cs
+++ SP23.P03.Web/Controllers/Test.cs
@@ -4,8 +4,17 @@ namespace SP23.P03.Web.Controllers
 {
     [ApiController]
     [Route("api/Test")]
-    public class Test
+    public class Test : ControllerBase
     {
         // This is to test my branch and repo since its buggy right now
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM login)
+        {
+
+        }
     }
 }
